137d5188c576e70bde40ba4edaf4b18ca26c123c
ogu <ken@ken-VGN-NW71FB-N.(none)>
2012-12-30 19:42:44 +0900

    correct Report.cpp

--- Report/Report.cpp
+++ Report/Report.cpp
@@ -2,8 +2,9 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
-int loop[105000], nice_n[105000], last[105000], bit[105000], n, parent[105000];
+
 vector<int> child[105000];
+int parent[105000], loop[105000], bit[105000], last[105000], nice_n[105000], n;
 bool come[105000];
 
 int comp(int x){
@@ -12,32 +13,31 @@ int comp(int x){
     int res = -1;
     come[x] = true;
     for(int i = 0;i < child[x].size();i++){
-	int t = comp(child[x][i]);
-	if(t != -1)res = t;
+	int tmp = comp(child[x][i]);
+	if(tmp != -1)res = tmp;
     }
-    if(res == -1 || res == x)loop[x] = x;
-    else{
+    loop[x] = x;
+    if(res == x)return -1;
+    if(res != -1){
 	loop[x] = res;
 	for(int i = 0;i < child[x].size();i++){
 	    child[res].push_back(child[x][i]);
 	}
     }
     come[x] = false;
-    if(x == res)return -1;
-    else return res;
+    return res;
 }
 
-int nice_node(int x, int n = 0){
+int nice_node(int x, int p){
     x = loop[x];
-    if(nice_n[x] != 0)return n;
-    n++;
-    nice_n[x] = n;
-    cout << n << endl;
+    if(nice_n[x] != 0)return p;
+    p++;
+    nice_n[x] = p;
     for(int i = 0;i < child[x].size();i++){
-	n = nice_node(child[x][i], n);
+	p = nice_node(child[x][i], p);
     }
-    last[x] = n;
-    return n;
+    last[x] = p;
+    return p;
 }
 
 int sum(int x){
@@ -50,17 +50,18 @@ int sum(int x){
 }
 
 int solve(int x){
-    if(nice_n[x] <= 0){cout << last[x] << endl;exit(0);}
     return sum(last[x]) - sum(nice_n[x] - 1);
 }
 
 void report(int x){
     x = nice_n[x];
-    while(x < 105000){
+    while(x <= 105000){
 	bit[x]++;
 	x += x & -x;
     }
 }
+
+
 
 int main(){
     cin >> n;
@@ -68,28 +69,21 @@ int main(){
 	cin >> parent[i];
 	child[parent[i]].push_back(i);
     }
-    for(int i = 1;i <= n;i++){
-	fill(come, come + 105000, false);
-	if(loop[i] == 0)comp(i);
-    }
+
+    for(int i = 1;i <= n;i++)
+	comp(i);   
+
     int p = 0;
     for(int i = 1;i <= n;i++){
-	if(nice_n[loop[i]] == 0){
-	    while(loop[i] == i)i = parent[i];	    
-	    p = nice_node(loop[i], p);
+	int j = i;
+	if(nice_n[loop[j]] == 0){
+	    while(loop[j] == j)j = parent[j];	    
+	    p = nice_node(j, p);
 	}
     }
+
     for(int i = 1;i <= n;i++){
-	/*cout <<*/ solve(loop[i]) /*<< endl*/;
+	cout << solve(loop[i]) << endl;
 	report(loop[i]);
     }
 }
-/*
-
-2 4 6
-
-  3
-
- 1 5
-
- */
